405927d754aec71b7cafdd5a7318ff8fda5fe944
JustinRhodes14 <rhodesju21@gmail.com>
2020-08-17 16:32:23 -0400

    Removed some comments

--- bot.py
+++ bot.py
@@ -1,24 +1,11 @@
 #BUGS
-#RTX doesnt work since it returns None for its company profile I'm assuming (for both news and quote)
-#Some stocks don't have pictures (DAL), should use a placeholder value (same with above RTX bug)
+#------
 
 #Individual peoples portfolios - Ehhh
     #Show current prices of peoples lists (only current prices) - Big task, need database (server)
-
-#Analyze
-    #Robinhood analytics - Recommendation Trends
-
-#History of P/E ratio
 #Alerts? - Maybe use yahoo
-#More data
-    #Market cap, outstanding shares, dividends, quarterly based, earnings reports, announced next earnings reports?
-    #Equity to Debt ratio
 #General purpose queries
     #Top movers, bottom movers, range, (bottom x top x)
-#News Sentiment
-
-#UP NEXT
-#News sentiment anaylsis (graph) ,  (recommendation trends) Analysis (robinhood thing) - maybe make a graph, top/bottom movers
 import os
 import random
 from dotenv import load_dotenv
